f9465cf6e453c7e7a7a25327cd4e379d2dd094a1
namba <namba.hiroki.z47@kyoto-u.jp>
2024-01-06 23:39:55 +0900

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,23 +1,21 @@
-"""
-名前一括変更
-"""
-
-from pathlib import Path  #同階層のpngとjpegをjpg変換する
+from pathlib import Path  
 import os
-import glob  #これで一括取得
+import glob  
+import re
 
-#同階層のpngとjpegをjpg変換する
 for f in Path('.').rglob('*.png'):
     f.rename(f.stem+'.jpg')
 
 for f in Path('.').rglob('*.jpeg'):
     f.rename(f.stem+'.jpg')
 
-#jpg取得
-files = glob.glob("*.jpg")
+def atoi(text):
+    return int(text) if text.isdigit() else text
 
-#例　()'.data/images/*.jpg')
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
+files = sorted(glob.glob("*.jpg"), key=natural_keys)
 
 for i, old_name in enumerate(files):
     os.rename(old_name,'img' + str(i+1) + '.jpg')
