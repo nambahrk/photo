cb53ce6f2cd7ffafb6ad38039f1e242c56173a0d
namba <namba.hiroki.z47@kyoto-u.jp>
2024-01-29 00:43:53 +0900

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 def tojpg():
     for f in Path('.').rglob('*.png'):
@@ -11,3 +12,18 @@ def topng():
         f.rename(f.stem+'.png')
     for f in Path('.').rglob('*.jpeg'):
         f.rename(f.stem+'.png')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python extension.py extension")
+        print("Valid operations: tojpg topng")
+    else:
+        operation = sys.argv[1]
+
+        if operation == "tojpg":
+            tojpg()
+        elif operation == "topng":
+            topng()       
+        else:
+            print("Invalid operation. Valid operations: blur, bright, compress, grayscale, monochrome, rotate")
