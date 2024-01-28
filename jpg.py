d94024c8f3c8fd9f365530a236d407672f290b9f
namba <namba.hiroki.z47@kyoto-u.jp>
2024-01-29 00:38:55 +0900

    Delete jpg.py

--- jpg.py
+++
@@ -1,9 +0,0 @@
-#同階層のpngとjpegをjpg変換する
-
-from pathlib import Path
-
-for f in Path('.').rglob('*.png'):
-    f.rename(f.stem+'.jpg')
-
-for f in Path('.').rglob('*.jpeg'):
-    f.rename(f.stem+'.jpg')
