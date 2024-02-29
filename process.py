3300c52c0c7c11c84c893a04aeb0f82613c6eecb
namba <namba.hiroki.z47@kyoto-u.jp>
2024-02-29 11:46:34 +0900

    Update process.py

--- process.py
+++ process.py
@@ -1,6 +1,9 @@
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance #Pillow
+import cv2 #OpenCV
 import sys
-import os
+import numpy as np
+from pathlib import Path
+
 
 def blur(img):
     img_blur = img.filter(ImageFilter.GaussianBlur(8))
@@ -28,17 +31,35 @@ def rotate(img):
     img_rotate = img.rotate(90, expand=True)
     img_rotate.save("rotate.jpg")    
 
+def anime(img):
+    k=20
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # Apply blur and edge detection
+    edge = cv2.blur(gray, (3, 3))
+    edge = cv2.Canny(edge, 50, 150, apertureSize=3)
+    edge = cv2.cvtColor(edge, cv2.COLOR_GRAY2BGR)
+
+    # Adjust image intensity
+    img = np.array(img / k, dtype=np.uint8)
+    img = np.array(img * k, dtype=np.uint8)
+
+    # Subtract edge from the original image
+    anime = cv2.subtract(img, edge)
+
+    cv2.imwrite("./anime.jpg", anime)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python script.py input_image operation")
-        print("Valid operations: blur, bright, compress, grayscale, monochrome, rotate")
+        print("Usage: python process.py input_image operation")
+        print("Valid operations: blur, bright, compress, grayscale, monochrome, rotate, anime, opacity")
     else:
         input_image_path = sys.argv[1]
         operation = sys.argv[2]
 
         img = Image.open(input_image_path)
+        img2 = cv2.imread(input_image_path)
 
         if operation == "blur":
             blur(img)
@@ -52,5 +73,8 @@ if __name__ == "__main__":
             monochrome(img)    
         elif operation == "rotate":
             rotate(img)               
+        elif operation == "anime":
+            anime(img2)    
         else:
-            print("Invalid operation. Valid operations: blur, bright, compress, grayscale, monochrome, rotate")
+            print("Invalid operation. Valid operations: blur, bright, compress, grayscale, monochrome, rotate, anime, opacity")
+
